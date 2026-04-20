38d911a892720b248682c3e20acf9781fc24fa48
Hernan Lopez <hlhernano@gmail.com>
2026-04-20 09:23:34 -0300

    Estructura

--- src/main/java/ucu/edu/aed/tda/Implementaciones/ArbolBinario.java
+++ src/main/java/ucu/edu/aed/tda/Implementaciones/ArbolBinario.java
@@ -1,5 +1,7 @@
 package ucu.edu.aed.tda.Implementaciones;
 
+import java.util.function.Consumer;
+
 public class ArbolBinario implements ucu.edu.aed.tda.Interfaces.TDAArbolBinario {
 
     @Override
@@ -24,6 +26,48 @@ public class ArbolBinario implements ucu.edu.aed.tda.Interfaces.TDAArbolBinario 
     public boolean insertar(Comparable dato) {
         // TODO Auto-generated method stub
         throw new UnsupportedOperationException("Unimplemented method 'insertar'");
+    }
+
+    @Override
+    public void inOrder(Consumer consumidor) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'inOrder'");
+    }
+
+    @Override
+    public void preOrder(Consumer consumidor) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'preOrder'");
+    }
+
+    @Override
+    public void postOrder(Consumer consumidor) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'postOrder'");
+    }
+
+    @Override
+    public boolean esVacio() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'esVacio'");
+    }
+
+    @Override
+    public int cantidadNodos() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'cantidadNodos'");
+    }
+
+    @Override
+    public int cantidadHojas() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'cantidadHojas'");
+    }
+
+    @Override
+    public int cantidadNodosInternos() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'cantidadNodosInternos'");
     }
     
 }
