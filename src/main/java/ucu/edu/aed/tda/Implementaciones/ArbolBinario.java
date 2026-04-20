5195b718c85de18de4b3171b7d5a7436028a8437
Hernan Lopez <hlhernano@gmail.com>
2026-04-20 09:20:40 -0300

    Estructura

---
+++ src/main/java/ucu/edu/aed/tda/Implementaciones/ArbolBinario.java
@@ -0,0 +1,29 @@
+package ucu.edu.aed.tda.Implementaciones;
+
+public class ArbolBinario implements ucu.edu.aed.tda.Interfaces.TDAArbolBinario {
+
+    @Override
+    public Object buscar(Comparable predicate) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'buscar'");
+    }
+
+    @Override
+    public Object obtenerRaiz() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'obtenerRaiz'");
+    }
+
+    @Override
+    public boolean eliminar(Comparable criterioBusqueda) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'eliminar'");
+    }
+
+    @Override
+    public boolean insertar(Comparable dato) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'insertar'");
+    }
+    
+}
---
+++ src/main/java/ucu/edu/aed/tda/Implementaciones/Elemento.java
@@ -0,0 +1,117 @@
+package ucu.edu.aed.tda.Implementaciones;
+
+import java.util.function.Consumer;
+
+import ucu.edu.aed.tda.Interfaces.TDAElemento;
+
+public class Elemento implements ucu.edu.aed.tda.Interfaces.TDAElemento {
+
+    @Override
+    public void setHijoIzquierdo(TDAElemento hijoIzquierdo) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'setHijoIzquierdo'");
+    }
+
+    @Override
+    public void setHijoDerecho(TDAElemento hijoDerecho) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'setHijoDerecho'");
+    }
+
+    @Override
+    public TDAElemento getHijoIzquierdo() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getHijoIzquierdo'");
+    }
+
+    @Override
+    public TDAElemento getHijoDerecho() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getHijoDerecho'");
+    }
+
+    @Override
+    public void setDato(Object dato) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'setDato'");
+    }
+
+    @Override
+    public Object getDato() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getDato'");
+    }
+
+    @Override
+    public TDAElemento buscar(Comparable criterioBusqueda) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'buscar'");
+    }
+
+    @Override
+    public TDAElemento eliminar(Comparable criterioBusqueda) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'eliminar'");
+    }
+
+    @Override
+    public boolean insertar(Comparable nuevoDato) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'insertar'");
+    }
+
+    @Override
+    public void inOrder(Consumer consumidor) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'inOrder'");
+    }
+
+    @Override
+    public void preOrder(Consumer consumidor) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'preOrder'");
+    }
+
+    @Override
+    public void postOrder(Consumer consumidor) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'postOrder'");
+    }
+
+    @Override
+    public boolean esHoja() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'esHoja'");
+    }
+
+    @Override
+    public int cantidadHojas() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'cantidadHojas'");
+    }
+
+    @Override
+    public int cantidadNodosInternos() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'cantidadNodosInternos'");
+    }
+
+    @Override
+    public int cantidadNodos() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'cantidadNodos'");
+    }
+
+    @Override
+    public int altura() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'altura'");
+    }
+
+    @Override
+    public int obtenerNivel(Comparable criterioBusqueda) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'obtenerNivel'");
+    }
+    
+}
